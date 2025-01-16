42411dd1607dc7ff4d7cbd0f8eea246b31417b8d
Raja Dhoundiyal <rajadhoundiyalpda5@gmail.com>
2025-01-16 12:58:01 +0530

    ui done

--- src/component/main/Main.jsx
+++ src/component/main/Main.jsx
@@ -5,32 +5,67 @@ const Main = () => {
   return (
     <div className="main flex-1 position-relative min-h-screen pb-[15vh] p-5 text-[#585858]">
       <div className="flex justify-between items-center ">
-        <p>Gemini</p>
+        <p className="text-lg font-normal">Gemini</p>
         <img src={assets.user_icon} alt="" className="w-10 rounded-full" />
       </div>
-      <div className="mainContainer max-w-4xl m-auto bg-red-50">
+      <div className="mainContainer max-w-4xl m-auto">
         <div className="gret my-12 mx-0 text-5xl text-[#c4c7c5] font-medium p-5">
-            <p><span>Hello, Raja</span></p>
-            <p>How can I help you today?</p>
+          <p>
+            <span className="text-red-300 font-semibold">Hello, Raja.</span>
+          </p>
+          <p className="mt-2 font-semibold">How can I help you today?</p>
         </div>
-        <div className="cards">
-          <div className="card">
-            <p>Suggest beautiful places to see on an upcoming road trip</p>
-            <img src={assets.compass_icon}/>
+        <div className="cards grid grid-cols-4 gap-4 p-4">
+          <div className="card h-48 bg-[#f0f4f9] p-4 rounded-xl relative cursor-pointer hover:bg-[#dfe4ea]">
+            <p className="text-[#6c6a6a]">
+              Suggest beautiful places to see on an upcoming road trip
+            </p>
+            <img
+              src={assets.compass_icon}
+              className="w-10 p-1 absolute bg-white rounded-full bottom-2 right-2"
+            />
           </div>
-          <div className="card">
-            <p>SUggest beautiful places to see on an upcoming road trip</p>
-            <img src={assets.bulb_icon}/>
+          <div className="card h-48 bg-[#f0f4f9] p-4 rounded-xl relative cursor-pointer hover:bg-[#dfe4ea]">
+            <p className="text-[#6c6a6a] ">
+              Suggest beautiful places to see on an upcoming road trip
+            </p>
+            <img
+              src={assets.bulb_icon}
+              className="w-10 p-1 absolute bg-white rounded-full bottom-2 right-2"
+            />
           </div>
-          <div className="card">
-            <p>SUggest beautiful places to see on an upcoming road trip</p>
-            <img src={assets.message_icon}/>
+          <div className="card h-48 bg-[#f0f4f9] p-4 rounded-xl relative cursor-pointer hover:bg-[#dfe4ea]">
+            <p className="text-[#6c6a6a] ">
+              Suggest beautiful places to see on an upcoming road trip
+            </p>
+            <img
+              src={assets.message_icon}
+              className="w-10 p-1 absolute bg-white rounded-full bottom-2 right-2"
+            />
           </div>
-          <div className="card">  
-            <p>SUggest beautiful places to see on an upcoming road trip</p>
-            <img src={assets.code_icon}/>
+          <div className="card h-48 bg-[#f0f4f9] p-4 rounded-xl relative cursor-pointer hover:bg-[#dfe4ea]">
+            <p className="text-[#6c6a6a] ">
+              Suggest beautiful places to see on an upcoming road trip
+            </p>
+            <img
+              src={assets.code_icon}
+              className="w-10 p-1 absolute bg-white rounded-full bottom-2 right-2"
+            />
           </div>
-
+        </div>
+        <div className="mainbottom absolute bottom-0 w-full max-w-4xl py-0 px-5">
+          <div className=" flex items-center justify-between gap-5 bg-[#F0F4F9] p-3 rounded-2xl">
+            <input type="text" placeholder="Enter a prompt here" className="border-none outline-none text-md bg-transparent p-1 flex-1" />
+            <div className="flex justify-between items-center gap-2.5 cursor-pointer">
+              <img src={assets.gallery_icon} alt="" className="w-6 h-6"/>
+              <img src={assets.mic_icon} alt="" className="w-6 h-6"/>
+              <img src={assets.send_icon} alt="" className="w-6 h-6"/>
+            </div>
+          </div>
+          <p className="bottominfo text-md my-4 mx-0 text-center font-medium">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa
+            possimus, minus autem sequi exceptu
+          </p>
         </div>
       </div>
     </div>
